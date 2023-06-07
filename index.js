d064d236a69f8ca9a1781a232962688a4692135b
Maxwell Alvord <maxwellalvord@gmail.com>
2023-06-07 12:05:17 -0700

    Update: add switch statement with banned words.

--- index.js
+++ index.js
@@ -1,12 +1,40 @@
 export const CheckName = (name) => {
   switch(name) {
     case "antidisestablishmentarianism":
-      console.error("Sorry that name has been blocked by the server host")
-      break;
-    case y:
-      // code block
-      break;
+      throw Error("Sorry that name has been blocked by the server host");
+    case ".adSensepostnottherenonobook":
+      throw Error("Sorry that name has been blocked by the server host");
+    case "<invalid>hello.html":
+      throw Error("Sorry that name has been blocked by the server host");
+    case "actSensepostnottherenonotive":
+      throw Error("Sorry that name has been blocked by the server host");
+    case "acunetix-wvs-test-for-some-inexistent-file":
+      throw Error("Sorry that name has been blocked by the server host");
+    case "appscan_fingerprint/mac_address":
+      throw Error("Sorry that name has been blocked by the server host");
+    case "arachni-":
+      throw Error("Sorry that name has been blocked by the server host");
+    case "cybercop":
+      throw Error("Sorry that name has been blocked by the server host");
+    case "nessus_is_probing_you_":
+      throw Error("Sorry that name has been blocked by the server host");
+    case "nessustest":
+      throw Error("Sorry that name has been blocked by the server host");
+    case "netsparker-":
+      throw Error("Sorry that name has been blocked by the server host");
+    case "rfiinc.txt":
+      throw Error("Sorry that name has been blocked by the server host");
+    case "thereisnowaythat-you-canbethere":
+      throw Error("Sorry that name has been blocked by the server host");
+    case "w3af/remotefileinclude.html":
+      throw Error("Sorry that name has been blocked by the server host");
+    case "appscan_fingerprint":
+      throw Error("Sorry that name has been blocked by the server host");
+    case "w00tw00t.at.ISC.SANS.DFind":
+      throw Error("Sorry that name has been blocked by the server host");
+    case "w00tw00t.at.blackhats.romanian.anti-sec":
+      throw Error("Sorry that name has been blocked by the server host");
     default:
-      // code block
+
   }
 }
