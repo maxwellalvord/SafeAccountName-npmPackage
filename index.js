086f5f8a2626bd3e01e2068af78ee19fd9292057
Maxwell Alvord <maxwellalvord@gmail.com>
2023-06-07 11:48:40 -0700

    First commit with boiler plate code.

---
+++ index.js
@@ -0,0 +1,12 @@
+export const CheckName = (name) => {
+  switch(name) {
+    case "antidisestablishmentarianism":
+      console.error("Sorry that name has been blocked by the server host")
+      break;
+    case y:
+      // code block
+      break;
+    default:
+      // code block
+  }
+}
